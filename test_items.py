86bf6a3cab4527bc0905a8bcce396216c8281824
dchuguntsev <dchuguntsev@htc-cs.ru>
2020-05-19 18:42:08 +0400

    Change test name

--- test_items.py
+++ test_items.py
@@ -1,4 +1,4 @@
-def test_multilanguage(browser):
+def test_add_to_card_button(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     assert browser.find_element_by_css_selector(".btn-add-to-basket")
